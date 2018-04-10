c979242cb8a05d2fa31e8fd5d5f839f4ad0bdc30
Domen Gašperlin <domen.gasperlin@gmail.com>
2018-04-10 18:17:18 +0200

    1.0-SNAPSHOT version

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # KumuluzEE Ethereum
-KumuluzEE Ethereum project for seamless development of Blockchain applications, smart contracts and distribute ledger and integration with Etherum platform. 
+
+KumuluzEE Ethereum project for seamless development of Blockchain applications, smart contracts, distributed ledger and integration with Etherum platform.
+Pre-release version enables you to easily interact with Ethereum network using web3j. Web3j documentation can be found [here](https://docs.web3j.io/).
+
+## Usage
+
+You can enable KumuluzEE Ethereum support by adding the following dependency to pom.xml:
+```xml
+<dependency>
+    <groupId>com.kumuluz.ee.ethereum</groupId>
+    <artifactId>kumuluzee-ethereum</artifactId>
+    <version>${kumuluzee-ethereum.version}</version>
+</dependency>
+```
+
+## Connecting to network
+In order to connect to the ethereum network you need to use a client. You can choose to run one yourself such as [geth](https://github.com/ethereum/go-ethereum/wiki/geth) or use client in the cloud such as [infura](https://infura.io/signup).
+For testing purposes we recommend you connect to testnet (rovan, kovan or rinekby). In this guide we will use rinkeby. To get free test ether use [rinkeby faucet](https://faucet.rinkeby.io/)
+
+## Creating a wallet
+
+In order to make transactions you need to have a wallet. You can use your existing wallet or get a new one. You can also create a wallet using ethereum client.
+
+Most popular options include:
+* [MyEtherWallet (Online)](https://www.myetherwallet.com)
+* [MetaMask](https://metamask.io)
+* [Mist (Desktop)](https://github.com/ethereum/mist/releases)
+* [Parity (Desktop)](https://ethcore.io/parity.html)
+
+If you want to perform operations using your account you will need to provide wallet data in config.yaml.
+
+### Configuration
+To configure web3j instance create configuration in resources/config.yaml.
+Here you can put path to your wallet and address of the client used to connect to the ethereum network.
+```yaml
+kumuluzee:
+  ethereum:
+    wallet:
+      path: "/path/to/wallet.json"
+      password: "kumuluzee"
+    client:
+      address: "http://localhost:8545"
+```
+### Contracts
+If you wish do deploy or interact with Solidity smart contracts you should place them in resources folder.
+
+## Usage
+You can obtain [web3j instance](https://docs.web3j.io/quickstart.html) by using Web3jUtils.
+```
+private Web3jUtils web3jUtils = Web3jUtils.getInstance();
+```
+You can also inject web3j instance using CDI.
+```
+    @Inject
+    @Web3jUtil
+    private Web3j web3j;
+```
+## License
+
+MIT
---
+++ web3j/src/main/java/com/kumuluz/ee/ethereum/utils/Web3jUtils.java
@@ -0,0 +1,95 @@
+/*
+ *  Copyright (c) 2014-2018 Kumuluz and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+package com.kumuluz.ee.ethereum.utils;
+
+import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.Transfer;
+import org.web3j.utils.Convert;
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+/**
+ * Web3jUtil class used for getting instance of Web3jUtil, web3j and credentials.
+ *
+ * @author Domen Gašperlin
+ * @since 1.0.0
+ */
+public class Web3jUtils {
+
+    private static Web3jUtils web3jUtils;
+    private Web3j web3j;
+    private static Credentials credentials;
+    private static final Logger log = Logger.getLogger(Web3jUtils.class.getName());
+
+    protected Web3jUtils() {
+        ConfigurationUtil configurationUtil = ConfigurationUtil.getInstance();
+        String clientAddress = configurationUtil.get("kumuluzee.ethereum.client.address").orElse("http://localhost:8545");
+        this.web3j = Web3j.build(new HttpService(clientAddress));
+        try {
+            credentials = loadWallet();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void initialize() {
+        web3jUtils = new Web3jUtils();
+    }
+
+    public static Web3jUtils getInstance() {
+        if (web3jUtils == null) {
+            throw new IllegalStateException("The Web3jUtil was not yet initialized.");
+        }
+        return web3jUtils;
+    }
+
+    public Web3j getWeb3j() {
+        return web3j;
+    }
+
+    public static Credentials getCredentials() {
+        if (credentials == null) {
+            throw new IllegalStateException("The Credentials were not yet provided.");
+        }
+        return credentials;
+    }
+
+    public Credentials loadWallet() throws Exception {
+        Optional walletPath = ConfigurationUtil.getInstance().get("kumuluzee.ethereum.wallet.path");
+        Optional walletPassword = ConfigurationUtil.getInstance().get("kumuluzee.ethereum.wallet.password");
+        if (walletPassword.isPresent() && walletPassword.isPresent()) {
+            return WalletUtils.loadCredentials(String.valueOf(walletPassword.get()), String.valueOf(walletPath.get()));
+        } else {
+            log.info("Wallet was not loaded, no path or credentials supplied.");
+            return null;
+        }
+    }
+}
